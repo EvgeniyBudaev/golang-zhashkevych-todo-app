02f782f7c5fe6b35ebff26515b33c243484fcda2
Evgeniy Budaev <budaev.e@gmail.com>
2023-09-19 13:13:41 +0300

    feat: http-сервер

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/EvgeniyBudaev/golang-zhashkevych-todo-app/internal/app/server"
+	"github.com/EvgeniyBudaev/golang-zhashkevych-todo-app/pkg/handler"
+	"log"
+)
 
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(server.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatal("error occured while running http server: %s", err.Error())
+	}
 }
